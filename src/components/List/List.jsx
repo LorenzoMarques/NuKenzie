5b2caae65de84247c6597228ec88c743cdd542a9
Lorenzo Marques <lorenzo.marques246@gmail.com>
2022-02-07 13:02:56 -0300

    Layout atualizado

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -5,6 +5,12 @@ const List = ({ listTransactions }) => {
   if (listTransactions[0] !== undefined) {
     return (
       <div className="list">
+        <div className="resumo-financeiro">
+          <h3>Resumo financeiro</h3>
+          <button>Todos</button>
+          <button>Entradas</button>
+          <button>Despesas</button>
+        </div>
         {listTransactions.map((transaction, index) => {
           return <Card transaction={transaction} key={index} />;
         })}
@@ -13,6 +19,12 @@ const List = ({ listTransactions }) => {
   } else {
     return (
       <div className="list">
+        <div className="resumo-financeiro">
+          <h3>Resumo financeiro</h3>
+          <button>Todos</button>
+          <button>Entradas</button>
+          <button>Despesas</button>
+        </div>
         <h2>Você ainda não possui nenhum lançamento</h2>
         <div className="empty-card">
           <div className="empy-card-ractangle1"></div>
